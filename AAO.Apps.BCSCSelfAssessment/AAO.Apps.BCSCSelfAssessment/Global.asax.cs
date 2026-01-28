10fe13860b3da71f654f42c26343b05eb4d2db65
Girish <Girishkumar.Naranje@Harbingerrgoup.com>
2026-01-28 16:35:03 +0530

    Update all projects to .NET Framework 4.8.1 and add TLS 1.2 support

--- AAO.Apps.BCSCSelfAssessment/AAO.Apps.BCSCSelfAssessment/Global.asax.cs
+++ AAO.Apps.BCSCSelfAssessment/AAO.Apps.BCSCSelfAssessment/Global.asax.cs
@@ -11,6 +11,9 @@
     {
         protected void Application_Start()
         {
+            // Enable TLS 1.2 and TLS 1.3 for secure connections
+            System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12 | System.Net.SecurityProtocolType.Tls13;
+
             ViewEngines.Engines.Clear();
             ViewEngines.Engines.Add(new RazorViewEngine());
             AreaRegistration.RegisterAllAreas();
@@ -18,38 +21,6 @@
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
-
-/*
-        protected void Application_BeginRequest()
-        {
-            HttpCookie sessionTimeout;
-            Uri myReferrer = Request.UrlReferrer;
-            string pageRefferrer = Convert.ToString(myReferrer);
-            if (!string.IsNullOrEmpty(pageRefferrer) && pageRefferrer.Contains("SignOut"))
-            {
-                Response.Cookies["SessionUnixTimeoutExpireAt"].Expires = DateTime.Now.AddDays(-1);
-            }
-            else
-            {
-                if (Request.Cookies["SessionUnixTimeoutExpireAt"] == null)
-                {
-                    sessionTimeout = new HttpCookie("SessionUnixTimeoutExpireAt");
-                }
-                else
-                {
-                    sessionTimeout = Request.Cookies["SessionUnixTimeoutExpireAt"];
-                }
-
-                //DateTime expireAt = DateTime.Now.AddMinutes(5).ToUniversalTime();
-                DateTime expireAt = DateTime.Now.Add(FormsAuthentication.Timeout).ToUniversalTime();
-                sessionTimeout.Value = expireAt.ToString("MM/dd/yyyy HH:mm:ss");
-
-                //sessionTimeout.Expires = DateTime.Now.AddMinutes(5);
-                sessionTimeout.Expires = DateTime.Now.Add(FormsAuthentication.Timeout);
-                Response.Cookies.Add(sessionTimeout);
-            }
-        }
-*/
 
         protected void Application_EndRequest()
         {
--- AAO.WebAPI.BCSCSelfAssessment/AAO.WebAPI.BCSCSelfAssessment/Global.asax.cs
+++ AAO.WebAPI.BCSCSelfAssessment/AAO.WebAPI.BCSCSelfAssessment/Global.asax.cs
@@ -7,21 +7,15 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
-//using OpenTelemetry;
-//using OpenTelemetry.Trace;
-//using Honeycomb.OpenTelemetry;
-//using OpenTelemetry.Instrumentation.AWSLambda;
-//using OpenTelemetry.Resources;
-//using OpenTelemetry.Trace;
-
 namespace AAO.WebAPI.BCSCSelfAssessment
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
-        //private TracerProvider _tracerProvider;
-
         protected void Application_Start()
         {
+            // Enable TLS 1.2 and TLS 1.3 for secure connections
+            System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12 | System.Net.SecurityProtocolType.Tls13;
+
             ViewEngines.Engines.Clear();
             ViewEngines.Engines.Add(new RazorViewEngine());
             AreaRegistration.RegisterAllAreas();
@@ -29,20 +23,6 @@ namespace AAO.WebAPI.BCSCSelfAssessment
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-
-           /* _tracerProvider = Sdk.CreateTracerProviderBuilder()
-               .AddHoneycomb(new HoneycombOptions
-               {
-                    ServiceName = "my-app",
-                    ApiKey = "DFoPtp8tBmfOXvioJhTcSE",
-                    TracesEndpoint = "https://ui.honeycomb.io/girishkumar.naranje-gettingstarted/environments/test/send-data/v1/traces",
-               })
-               .Build();*/
         }
-
-        /*protected void Application_End()
-        {
-            _tracerProvider?.Dispose();
-        }*/
     }
 }
